dabb326ed36470c2d96a51aaf38bb2eb4913615f
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-06-07 20:52:39 -0400

    flask config

--- api/__init__.py
+++ api/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://cqtdgqvm:FbD22gPUCjOaD-i86tm1uJvQGP1L6_W5@ruby.db.elephantsql.com/cqtdgqvm"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://cqtdgqvm:FbD22gPUCjOaD-i86tm1uJvQGP1L6_W5@ruby.db.elephantsql.com/cqtdgqvm"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
