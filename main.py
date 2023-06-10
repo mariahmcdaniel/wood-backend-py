55523ac1ff979685a6264e7dfd2403965695dce9
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-06-09 22:31:53 -0400

    /graphql route defined

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ from ariadne import load_schema_from_path, make_executable_schema, \
 from ariadne.constants import PLAYGROUND_HTML
 from flask import request, jsonify
 
+type_defs = load_schema_from_path("schema.graphql")
+schema = make_executable_schema(
+    type_defs, snake_case_fallback_resolvers
+)
+
+@app.route("/graphql", methods=["GET"])
+def graphql_playground():
+    return PLAYGROUND_HTML, 200
