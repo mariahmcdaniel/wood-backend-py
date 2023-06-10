bf9b7085a2daf6645a40614867ee87d30eae809a
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-06-09 22:29:18 -0400

    importing ariadne & co

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 from api import app, db
-from api import models
+from ariadne import load_schema_from_path, make_executable_schema, \
+    graphql_sync, snake_case_fallback_resolvers, ObjectType
+from ariadne.constants import PLAYGROUND_HTML
+from flask import request, jsonify
+
