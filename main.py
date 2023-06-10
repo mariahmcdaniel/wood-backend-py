e228cfc35fbcfba033c3d7298bee9935588c401e
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-06-09 22:34:54 -0400

    /graphql route POST method defined

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ schema = make_executable_schema(
 @app.route("/graphql", methods=["GET"])
 def graphql_playground():
     return PLAYGROUND_HTML, 200
+    
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code     
