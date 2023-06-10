0a123b6960a3e43952a2aeff2ae4924cb8b9fb7f
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-06-09 23:12:30 -0400

    subbed explorer for playground

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from api import app, db
 from ariadne import load_schema_from_path, make_executable_schema, \
     graphql_sync, snake_case_fallback_resolvers, ObjectType
-from ariadne.constants import PLAYGROUND_HTML
+from ariadne.explorer import ExplorerGraphiQL
 from flask import request, jsonify
 
-type_defs = load_schema_from_path("schema.graphql")
+type_defs = load_schema_from_path("./schema.graphql")
 schema = make_executable_schema(
     type_defs, snake_case_fallback_resolvers
 )
 
+explorer_html = ExplorerGraphiQL().html(None)
+
 @app.route("/graphql", methods=["GET"])
-def graphql_playground():
-    return PLAYGROUND_HTML, 200
+def graphql_explorer():
+    return explorer_html, 200
     
 @app.route("/graphql", methods=["POST"])
 def graphql_server():
