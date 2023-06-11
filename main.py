36c42a71851102ff7f23ee8d59e632d8c5f8eb76
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-06-10 22:30:15 -0400

    imported resolver into main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ariadne import load_schema_from_path, make_executable_schema, \
     graphql_sync, snake_case_fallback_resolvers, ObjectType
 from ariadne.explorer import ExplorerGraphiQL
 from flask import request, jsonify
+from api.queries import listProjects_resolver
 
 type_defs = load_schema_from_path("./schema.graphql")
 schema = make_executable_schema(
